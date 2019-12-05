9938e692bb88e0b859b5be7054e2e615cd0e3969
jinghuazhao <jinghuazhao@hotmail.com>
2019-12-05 09:20:24 +0000

    st.sh

--- st.sh
+++ st.sh
@@ -1,9 +1,11 @@
-# 30-11-2019 JHZ
+# 5-12-2019 JHZ
 
 git add README.md
 git commit -m "README"
 git add BiSeq hearing
 git commit -m "Code"
+git add P.Rmd P.html
+git commit -m "P values"
 git add st.sh
 git commit -m "st.sh"
 git push
