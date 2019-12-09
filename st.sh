5aab09531c05e92b01e8738ffbd2e8b47a3d09f7
jinghuazhao <jinghuazhao@hotmail.com>
2019-12-09 10:38:11 +0000

    st.sh

--- st.sh
+++ st.sh
@@ -1,9 +1,10 @@
-# 5-12-2019 JHZ
+# 9-12-2019 JHZ
 
 git add README.md
 git commit -m "README"
 git add BiSeq hearing
 git commit -m "Code"
+module load gcc/5
 R --no-save <<END
   rmarkdown::render("P.Rmd", c("html_document", "pdf_document"))
 END
