38da1ec55386a1635f942d4f26846b8cc39116e9
jinghuazhao <jinghuazhao@hotmail.com>
2020-07-11 10:14:39 +0100

    replaced

--- HUA_methylation_analysis/split.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-export list=$(ls BiSeq/* | xargs -l basename -s .gz)
-export chrs=$(echo $(seq 22) X Y M)
-
-mkdir src
-parallel -j10 -C' ' 'zcat BiSeq/{1}.gz | awk -v chr=chr{2} '\$1==chr' | gzip -f > src/{1}_chr{2}.gz' ::: ${list} ::: ${chrs}
-parallel -j10 -C' ' 'zgrep "_" BiSeq/{1}.gz | gzip -f > src/{}_.gz' ::: ${list}
