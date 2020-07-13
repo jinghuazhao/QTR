3f0d025539c1f1e7a03fa24ff74e4f5d29f812de
jinghuazhao <jinghuazhao@hotmail.com>
2020-07-13 12:12:38 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -15,6 +15,7 @@ git add BiSeq hearing HUA_methylation_analysis/scripts* \
     HUA_methylation_analysis/README.md \
     HUA_methylation_analysis/BiSeq_1.28.1.tar.gz \
     HUA_methylation_analysis/BiSeq.* \
+    HUA_methylation_analysis/refactor_modify.R
     HUA_methylation_analysis/?_*.R
 git commit -m "Code"
 git add P.Rmd P.html P.pdf
