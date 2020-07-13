f5c7e0d606505644b3bf2a9e4de6fdfddedda3cb
jinghuazhao <jinghuazhao@hotmail.com>
2020-07-13 15:16:26 +0100

    Code

--- HUA_methylation_analysis/BiSeq.sh
+++ HUA_methylation_analysis/BiSeq.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
-function setup()
+function partition()
+# partition of data by chromosome
 {
   export list=$(ls BiSeq/* | xargs -l basename -s .gz)
   export chrs=$(echo $(seq 22) X Y M)
@@ -9,24 +10,32 @@ function setup()
   mkdir src
   parallel --env out -j10 -C' ' 'zcat BiSeq/{1}.gz | awk -v chr=chr{2} "\$1==chr" | gzip -f > ${out}/{1}_chr{2}.gz' ::: ${list} ::: ${chrs}
   parallel --env out -j10 -C' ' 'zgrep "_" BiSeq/{1}.gz | gzip -f > ${out}/{}_Z.gz' ::: ${list}
+
+  export suffix=lmer
+
+  export chrs=$(echo $(seq 22) X Y M Z)
+  for chromosome in ${chrs}
+  do
+    export chr=chr${chromosome}
+    export scripts_dir=scripts_${suffix}
+    echo --- running ${suffix} chromosome ${chromosome} ---
+    R --no-save -q < ${scripts_dir}/1_BiSeq.R > 1.log
+    R --no-save -q < ${scripts_dir}/2_format_methylation_data.R > 2.log
+    R --no-save -q < ${scripts_dir}/3_quality_control_and_refactor.R > 3.log
+    R --no-save -q < ${scripts_dir}/4_analysis_lmer.R > 4.log
+  done
 }
 
-export suffix=lmer
+function all()
+{
+  R --no-save -q < 1_BiSeq.R > 1.log
+  R --no-save -q < 2_format_methylation_data.R > 2.log
+  R --no-save -q < 3_quality_control_and_refactor.R > 3.log
+  R --no-save -q < 4_analysis_lmer.R > 4.log
+}
 
-export chrs=$(echo $(seq 22) X Y M Z)
-for chromosome in ${chrs}
-do
-  export chr=chr${chromosome}
-  export scripts_dir=scripts_${suffix}
-  echo --- running ${suffix} chromosome ${chromosome} ---
-  R --no-save -q < ${scripts_dir}/1_BiSeq.R > 1.log
-  R --no-save -q < ${scripts_dir}/2_format_methylation_data.R > 2.log
-  R --no-save -q < ${scripts_dir}/3_quality_control_and_refactor.R > 3.log
-  R --no-save -q < ${scripts_dir}/4_analysis_lmer.R > 4.log
-done
+# below for both partition and all
 R --no-save -q < ${scripts_dir}/5_read_result.R
 R --no-save -q < ${scripts_dir}/6_get_annotation.R
-R --no-save -q < ${scripts_dir}/7_combined-pvalues.sh
+7_combined-pvalues.sh
 R --no-save -q < ${scripts_dir}/8_sigplot.R
-                 
-
