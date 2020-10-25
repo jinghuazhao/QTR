8e7ba4116fbf797c625fa5022bf4ed34f4c444bb
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-10-25 14:32:48 +0000

    Code

--- depression_EWAS/init.sh
+++ depression_EWAS/init.sh
@@ -4,8 +4,11 @@ export src=BiSeq
 export prefix=CpG_context_Human_
 export suffix=_good_1_val_1_bismark_bt2_pe.bismark.cov.gz
 
-for n in $(ls ${src} | xargs -I {} sh -c "basename {} ${suffix} | sed 's/"${prefix}"//'")
+for n in $(ls ${src} | sed 's/'"${prefix}"'//;s/'"${suffix}"'//')
 do
   echo ${src}/${prefix}${n}${suffix}
   zcat ${src}/${prefix}${n}${suffix} | awk '/^chr[0-9]$|^chr[0-9][0-9]$|chrX|chrY/' | gzip -f > ${n}.gz
 done
+
+# more sophisticated
+# ls ${src} | xargs -I {} sh -c "basename {} ${suffix} | sed 's/"${prefix}"//'"
